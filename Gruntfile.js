24949c42871e68ddaa16ea9bf94ef64f330e991e
LAPTOP-QSODT5VR\Suneel <shsuneel@gmail.com>
2018-04-09 15:32:18 +0100

    dividing main page into components

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,9 @@
             files: {
               'src/pgn-app/comp/css/header.css': 'src/pgn-app/comp/scss/header.scss',
               'src/pgn-app/comp/css/app.css': 'src/pgn-app/comp/scss/app.scss',
-              'src/pgn-app/comp/css/plan.css': 'src/pgn-app/comp/scss/plan.scss'
+              'src/pgn-app/comp/css/productOverview.css': 'src/pgn-app/comp/scss/productOverview.scss',
+              'src/pgn-app/comp/css/plan.css': 'src/pgn-app/comp/scss/plan.scss',
+              'src/pgn-app/comp/css/keyFeatures.css': 'src/pgn-app/comp/scss/keyFeatures.scss'
             }
           }
         },
