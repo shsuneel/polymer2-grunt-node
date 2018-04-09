963361b38c27d509c873556c2b9ef0576b19dee3
LAPTOP-QSODT5VR\Suneel <shsuneel@gmail.com>
2018-04-09 13:01:41 +0100

    basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+
+  module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+          dist: {
+            options: {
+              style: 'expanded'
+            },
+            files: {
+              'src/pgn-app/comp/css/header.css': 'src/pgn-app/comp/scss/header.scss',
+              'src/pgn-app/comp/css/app.css': 'src/pgn-app/comp/scss/app.scss',
+              'src/pgn-app/comp/css/plan.css': 'src/pgn-app/comp/scss/plan.scss'
+            }
+          }
+        },
+        exec: {
+          cmd : 'polyserve'
+        },
+        watch: {
+          scripts: {
+            files: ['src/pgn-app/comp/scss/*.scss'],
+            tasks: ['sass'],
+            options: {
+              spawn: false,
+            },
+          },
+        }
+      });
+      
+      grunt.loadNpmTasks('grunt-contrib-sass');
+      grunt.loadNpmTasks('grunt-exec');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.registerTask('default', ['watch']);
+  };
